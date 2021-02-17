e694231b819ca229bcb2afb98e498d208e0e1fb1
rkterungwa16 <terungwakombol@gmail.com>
2021-02-17 22:51:04 +0100

    run draggable component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React from "react";
+
+import Draggable from "./Draggable";
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,18 +8,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Draggable>
+          <div>
+            <img src={logo} className="App-logo" alt="logo" />
+          </div>
+        </Draggable>
       </header>
     </div>
   );
