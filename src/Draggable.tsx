486a9054a524a64c07089732856ddb46068cdc58
rkterungwa16 <terungwakombol@gmail.com>
2021-02-18 19:58:20 +0100

    config code format with prettier

--- src/Draggable.tsx
+++ src/Draggable.tsx
@@ -1,79 +1,84 @@
 import React, { ReactElement } from "react";
 
 interface DraggableProps {
-    children: ReactElement<any>;
+  children: ReactElement<any>;
 }
 
 const Draggable = (props: DraggableProps) => {
-    const [currentPosition, setCurrentPosition] = React.useState({
-        x: 0,
-        deltaX: 0,
-        y: 0,
-        deltaY: 0
-    });
-    const [dragging, setDragging] = React.useState(false);
-    const draggableELement = React.useRef(null) as { [x: string]: any };
-    React.useEffect(() => {
-        if (currentPosition.x && currentPosition.y) {
-            draggableELement.current.style.top = (draggableELement.current.offsetTop - currentPosition.deltaY) + "px";
-            draggableELement.current.style.left = (draggableELement.current.offsetLeft - currentPosition.deltaX) + "px";
-        }
-    }, [
-        currentPosition,
-        currentPosition.x,
-        currentPosition.y,
-        currentPosition.deltaX,
-        currentPosition.deltaY
-    ]);
-
-    function onMouseDown(e: MouseEvent) {
-        e.preventDefault();
-        setCurrentPosition({
-            ...currentPosition,
-            x: e.clientX,
-            y: e.clientY
-        });
-        setDragging(true);
+  const [currentPosition, setCurrentPosition] = React.useState({
+    x: 0,
+    deltaX: 0,
+    y: 0,
+    deltaY: 0,
+  });
+  const [dragging, setDragging] = React.useState(false);
+  const draggableELement = React.useRef(null) as { [x: string]: any };
+  React.useEffect(() => {
+    if (currentPosition.x && currentPosition.y) {
+      draggableELement.current.style.top =
+        draggableELement.current.offsetTop - currentPosition.deltaY + "px";
+      draggableELement.current.style.left =
+        draggableELement.current.offsetLeft - currentPosition.deltaX + "px";
     }
-    const onMouseMove = React.useCallback((e) => {
-        e.preventDefault();
-        if (dragging) {
-            setCurrentPosition({
-                x: e.clientX,
-                deltaX: currentPosition.x - e.clientX,
-                y: e.clientY,
-                deltaY: currentPosition.y - e.clientY,
-            });
-        }
-    }, [currentPosition.x, currentPosition.y, dragging]);
+  }, [
+    currentPosition,
+    currentPosition.x,
+    currentPosition.y,
+    currentPosition.deltaX,
+    currentPosition.deltaY,
+  ]);
 
-    const onMouseUp = React.useCallback((e) => {
-        setDragging(false);
-    }, []);
+  function onMouseDown(e: MouseEvent) {
+    e.preventDefault();
+    setCurrentPosition({
+      ...currentPosition,
+      x: e.clientX,
+      y: e.clientY,
+    });
+    setDragging(true);
+  }
+  const onMouseMove = React.useCallback(
+    (e) => {
+      e.preventDefault();
+      if (dragging) {
+        setCurrentPosition({
+          x: e.clientX,
+          deltaX: currentPosition.x - e.clientX,
+          y: e.clientY,
+          deltaY: currentPosition.y - e.clientY,
+        });
+      }
+    },
+    [currentPosition.x, currentPosition.y, dragging]
+  );
 
-    React.useEffect(() => {
-        document.addEventListener("mousemove", onMouseMove);
-        document.addEventListener("mouseup", onMouseUp);
-        document.addEventListener("mouseleave", onMouseUp);
-        return function cleanup() {
-            document.removeEventListener("mousemove", onMouseMove);
-            document.removeEventListener("mouseup", onMouseUp);
-            document.removeEventListener("mouseleave", onMouseUp);
-        };
-    }, [onMouseMove, onMouseUp]);
+  const onMouseUp = React.useCallback((e) => {
+    setDragging(false);
+  }, []);
 
-    return (<React.Fragment>
-        {
-            React.cloneElement(props.children, {
-                ref: draggableELement,
-                onMouseDown,
-                style: {
-                    cursor: "move",
-                    position: "absolute"
-                }
-            })
-        }
-        </React.Fragment>)
-}
+  React.useEffect(() => {
+    document.addEventListener("mousemove", onMouseMove);
+    document.addEventListener("mouseup", onMouseUp);
+    document.addEventListener("mouseleave", onMouseUp);
+    return function cleanup() {
+      document.removeEventListener("mousemove", onMouseMove);
+      document.removeEventListener("mouseup", onMouseUp);
+      document.removeEventListener("mouseleave", onMouseUp);
+    };
+  }, [onMouseMove, onMouseUp]);
+
+  return (
+    <React.Fragment>
+      {React.cloneElement(props.children, {
+        ref: draggableELement,
+        onMouseDown,
+        style: {
+          cursor: "move",
+          position: "absolute",
+        },
+      })}
+    </React.Fragment>
+  );
+};
 
 export default Draggable;
